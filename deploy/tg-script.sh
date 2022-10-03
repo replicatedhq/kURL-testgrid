be468a1561276002f9dff422a77e84f787e88f82
Andrew Lavery <laverya@umich.edu>
2022-10-03 14:37:44 -0400

    use latest kubevirt and more recet k8s

--- deploy/tg-script.sh
+++ deploy/tg-script.sh
@@ -14,7 +14,7 @@ mount /dev/nvme0n1 /var/openebs/local/
 
 echo "Installing kURL"
 INSTALL_SCRIPT=/root/kurl-install.sh
-curl https://kurl.sh/f3dd2d4 > $INSTALL_SCRIPT
+curl https://kurl.sh/2e7f363 > $INSTALL_SCRIPT
 sed -i 's/parse_yaml_into_bash_variables$/parse_yaml_into_bash_variables\n    PRIVATE_ADDRESS=$(\/sbin\/ifconfig bond0:0 | awk "\/inet \/ {print \\$2}")/' $INSTALL_SCRIPT 
 chmod +x $INSTALL_SCRIPT
 ./$INSTALL_SCRIPT
@@ -22,17 +22,17 @@ chmod +x $INSTALL_SCRIPT
 cp /etc/kubernetes/admin.conf /root/.kube/config
 export KUBECONFIG=/root/.kube/config
 
-echo "Removing OpenEbs webhook"
-kubectl delete deployment openebs-admission-server -n openebs
+echo "Removing OpenEBS webhook"
+kubectl delete deployment openebs-admission-server -n openebs || true
 
-echo "Instaling KubeVirt"
+echo "Installing KubeVirt"
 kubectl create namespace kubevirt
-kubectl apply -f https://github.com/kubevirt/kubevirt/releases/download/v0.32.0/kubevirt-operator.yaml
-kubectl apply -f https://github.com/kubevirt/kubevirt/releases/download/v0.32.0/kubevirt-cr.yaml
+kubectl apply -f https://github.com/kubevirt/kubevirt/releases/download/v0.57.1/kubevirt-operator.yaml
+kubectl apply -f https://github.com/kubevirt/kubevirt/releases/download/v0.57.1/kubevirt-cr.yaml
 kubectl wait --timeout=180s --for=condition=Available -n kubevirt kv/kubevirt
 kubectl create ns cdi
-kubectl apply -f https://github.com/kubevirt/containerized-data-importer/releases/download/v1.24.0/cdi-operator.yaml
-kubectl apply -f https://github.com/kubevirt/containerized-data-importer/releases/download/v1.24.0/cdi-cr.yaml
+kubectl apply -f https://github.com/kubevirt/containerized-data-importer/releases/download/v1.55.0/cdi-operator.yaml
+kubectl apply -f https://github.com/kubevirt/containerized-data-importer/releases/download/v1.55.0/cdi-cr.yaml
 
 echo "Installing krew KubeVirt plugin"
 export HOME=/root
