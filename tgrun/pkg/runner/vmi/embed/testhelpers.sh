a562f2e1a7219138b72997836db93e72869765be
Ethan Mosbaugh <ethan@replicated.com>
2023-06-08 12:31:34 -0700

    fix: rhel 9 openssl bug (#335)

--- tgrun/pkg/runner/vmi/embed/testhelpers.sh
+++ tgrun/pkg/runner/vmi/embed/testhelpers.sh
@@ -71,7 +71,7 @@ function object_store_bucket_exists() {
     local acl="x-amz-acl:private"
     local d=$(LC_TIME="en_US.UTF-8" TZ="UTC" date +"%a, %d %b %Y %T %z")
     local string="HEAD\n\n\n${d}\n${acl}\n/$bucket"
-    local sig=$(echo -en "${string}" | openssl sha1 -hmac "${OBJECT_STORE_SECRET_KEY}" -binary | base64)
+    local sig=$(echo -en "${string}" | openssl dgst -sha1 -hmac "${OBJECT_STORE_SECRET_KEY}" -binary | base64)
 
     curl -fsSL -I \
         --globoff \
@@ -88,7 +88,7 @@ function _object_store_create_bucket() {
   local acl="x-amz-acl:private"
   local d=$(LC_TIME="en_US.UTF-8" TZ="UTC" date +"%a, %d %b %Y %T %z")
   local string="PUT\n\n\n${d}\n${acl}\n/$bucket"
-  local sig=$(echo -en "${string}" | openssl sha1 -hmac "${OBJECT_STORE_SECRET_KEY}" -binary | base64)
+  local sig=$(echo -en "${string}" | openssl dgst -sha1 -hmac "${OBJECT_STORE_SECRET_KEY}" -binary | base64)
   curl -fsSL -X PUT  \
     --globoff \
     --noproxy "*" \
@@ -117,7 +117,7 @@ function object_store_write_object() {
   local contentType="application/x-compressed-tar"
   local d=$(LC_TIME="en_US.UTF-8" TZ="UTC" date +"%a, %d %b %Y %T %z")
   local string="PUT\n\n${contentType}\n${d}\n${resource}"
-  local sig=$(echo -en "${string}" | openssl sha1 -hmac "${OBJECT_STORE_SECRET_KEY}" -binary | base64)
+  local sig=$(echo -en "${string}" | openssl dgst -sha1 -hmac "${OBJECT_STORE_SECRET_KEY}" -binary | base64)
 
   curl -X PUT -T "${file}" \
     --globoff \
@@ -136,7 +136,7 @@ function object_store_get_object() {
   local contentType="application/x-compressed-tar"
   local d=$(LC_TIME="en_US.UTF-8" TZ="UTC" date +"%a, %d %b %Y %T %z")
   local string="GET\n\n${contentType}\n${d}\n${resource}"
-  local sig=$(echo -en "${string}" | openssl sha1 -hmac "${OBJECT_STORE_SECRET_KEY}" -binary | base64)
+  local sig=$(echo -en "${string}" | openssl dgst -sha1 -hmac "${OBJECT_STORE_SECRET_KEY}" -binary | base64)
 
   curl -X GET -o "${file}" \
   --globoff \
