7a3ab5fc1397bbf1ea0b09f1be5bb47353d5d1ec
Andrew Lavery <laverya@umich.edu>
2023-09-02 00:21:01 +0200

    draft

--- tgrun/pkg/runner/vmi/embed/background.sh
+++ tgrun/pkg/runner/vmi/embed/background.sh
@@ -1,10 +1,24 @@
 #!/usr/bin/env bash
 
+function upload_remote_command() {
+    filepath=$1
+    contents=$(cat /var/lib/kurl/remotes/onenode/$filepath)
+    curl -X PUT -f -H "Content-Type: application/json" -d "{\"command\": \"$contents\"}" "$TESTGRID_APIENDPOINT/v1/instance/$NODE_ID/upgrade-command/$filepath"
+}
+
 function upload_remote_commands() {
+    export -f upload_remote_command
     while true
     do
            touch  ./lastwatch
            sleep 10
-           find /YOUR/WATCH/PATH -cnewer ./lastwatch -exec SOMECOMMAND {} \;
+           find /var/lib/kurl/remotes/onenode -cnewer ./lastwatch -exec bash -c "upload_remote_command \$1" {} \;
     done
 }
+
+function main()
+{
+    upload_remote_commands
+}
+
+main
