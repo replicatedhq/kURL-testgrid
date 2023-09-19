17eca2fa6f266d1c3cc5b3a193572cf508145fff
Andrew Lavery <laverya@umich.edu>
2023-09-18 21:52:40 -0400

    fix upload of scripts

--- tgrun/pkg/runner/vmi/embed/background.sh
+++ tgrun/pkg/runner/vmi/embed/background.sh
@@ -2,8 +2,19 @@
 
 function upload_remote_command() {
     filepath=$1
-    contents=$(cat /var/lib/kurl/remotes/onenode/$filepath)
-    curl -X PUT -f -H "Content-Type: application/json" -d "{\"command\": \"$contents\"}" "$TESTGRID_APIENDPOINT/v1/instance/$NODE_ID/upgrade-command/$filepath"
+
+    # if filepath is a directory, do not send it
+    if [ -d "$filepath" ]; then
+        return
+    fi
+
+    contents=$(cat $filepath)
+
+    # get last part of the filepath
+    local filename=
+    filename=$(basename -- "$filepath")
+
+    curl -X PUT -f -H "Content-Type: application/json" -d "{\"command\": \"$contents\"}" "$TESTGRID_APIENDPOINT/v1/instance/$NODE_ID/upgrade-command/$filename"
 }
 
 function upload_remote_commands() {
@@ -12,12 +23,13 @@ function upload_remote_commands() {
     do
            touch  ./lastwatch
            sleep 10
-           find /var/lib/kurl/remotes/onenode -cnewer ./lastwatch -exec bash -c "upload_remote_command \$1" {} \;
+           find /var/lib/kurl/remotes/onenode -cnewer ./lastwatch -exec bash -c "upload_remote_command \$0" {} \;
     done
 }
 
 function main()
 {
+    source /opt/kurl-testgrid/vars.sh
     upload_remote_commands
 }
 
