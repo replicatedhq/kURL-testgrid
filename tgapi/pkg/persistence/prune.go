a876ac260d930e5172f5e39ab647f98769e4a6ac
Andrew Lavery <laverya@umich.edu>
2024-01-24 10:48:03 -0500

    delete 1000 rows per table per day (#548)

--- tgapi/pkg/persistence/prune.go
+++ tgapi/pkg/persistence/prune.go
@@ -27,8 +27,17 @@ func PrunePG(pg *sql.DB, pruneDuration time.Duration) (int, int, error) {
 	deleteBefore := time.Now().Add(-pruneDuration * 3)
 
 	runDeleteQuery := `
-DELETE FROM testrun 
-WHERE ref = any (array(SELECT ref FROM testrun WHERE created_at < $1 ORDER BY created_at LIMIT 1000))`
+DELETE FROM testrun
+WHERE ref = any (
+	array(
+		SELECT ref
+		FROM testrun
+		WHERE created_at < $1
+		ORDER BY created_at
+		LIMIT 1000
+	)
+)
+`
 	result, err := pg.Exec(runDeleteQuery, deleteBefore)
 	if err != nil {
 		return -1, -1, fmt.Errorf("error deleting testrun entries: %v", err)
@@ -40,8 +49,22 @@ WHERE ref = any (array(SELECT ref FROM testrun WHERE created_at < $1 ORDER BY cr
 	deletedRows += int(deleted)
 	log.Printf("Deleted %d testrun entries", deleted)
 
-	// delete test instances that do not have a matching testrun
-	result, err = pg.Exec("DELETE FROM testinstance WHERE NOT EXISTS (SELECT FROM testrun WHERE testinstance.testrun_ref = testrun.ref)")
+	// delete 1000 test instances that do not have a matching testrun
+	instanceDeleteQuery := `
+DELETE FROM testinstance
+WHERE id = any (
+	array(
+		SELECT id FROM testinstance
+		WHERE NOT EXISTS (
+			SELECT FROM testrun
+			WHERE testinstance.testrun_ref = testrun.ref
+		)
+		ORDER BY enqueued_at
+		LIMIT 1000
+	)
+)
+`
+	result, err = pg.Exec(instanceDeleteQuery)
 	if err != nil {
 		return -1, -1, fmt.Errorf("error deleting testinstance entries: %v", err)
 	}
@@ -52,29 +75,55 @@ WHERE ref = any (array(SELECT ref FROM testrun WHERE created_at < $1 ORDER BY cr
 	deletedRows += int(deleted)
 	log.Printf("Deleted %d testinstance entries", deleted)
 
-	// TODO: reenable these deletion jobs at some point in the future when we are sure that the DB is in a good state
-	//// delete test upgrades/nodes that do not have a matching testinstance
-	//result, err = pg.Exec("DELETE FROM clusternode WHERE NOT EXISTS (SELECT FROM testinstance WHERE clusternode.testinstance_id = testinstance.id)")
-	//if err != nil {
-	//	return -1, -1, fmt.Errorf("error deleting clusternode entries: %v", err)
-	//}
-	//deleted, err = result.RowsAffected()
-	//if err != nil {
-	//	return -1, -1, fmt.Errorf("error getting rows affected after deleting clusternode entries: %v", err)
-	//}
-	//deletedRows += int(deleted)
-	//log.Printf("Deleted %d clusternode entries", deleted)
-	//
-	//result, err = pg.Exec("DELETE FROM testupgrade WHERE NOT EXISTS (SELECT FROM testinstance WHERE testupgrade.id = testinstance.id)")
-	//if err != nil {
-	//	return -1, -1, fmt.Errorf("error deleting testupgrade entries: %v", err)
-	//}
-	//deleted, err = result.RowsAffected()
-	//if err != nil {
-	//	return -1, -1, fmt.Errorf("error getting rows affected after deleting testupgrade entries: %v", err)
-	//}
-	//deletedRows += int(deleted)
-	//log.Printf("Deleted %d testupgrade entries", deleted)
+	// delete 1000 test upgrades/nodes that do not have a matching testinstance
+	nodeDeleteQuery := `
+DELETE FROM clusternode
+WHERE id = any (
+	array(
+		SELECT id FROM clusternode
+		WHERE NOT EXISTS (
+			SELECT FROM testinstance
+			WHERE clusternode.testinstance_id = testinstance.id
+		)
+		ORDER BY created_at
+		LIMIT 1000
+	)
+)
+`
+	result, err = pg.Exec(nodeDeleteQuery)
+	if err != nil {
+		return -1, -1, fmt.Errorf("error deleting clusternode entries: %v", err)
+	}
+	deleted, err = result.RowsAffected()
+	if err != nil {
+		return -1, -1, fmt.Errorf("error getting rows affected after deleting clusternode entries: %v", err)
+	}
+	deletedRows += int(deleted)
+	log.Printf("Deleted %d clusternode entries", deleted)
+
+	upgradeDeleteQuery := `
+DELETE FROM testupgrade
+WHERE id = any (
+	array(
+		SELECT id FROM testupgrade
+		WHERE NOT EXISTS (
+			SELECT FROM testinstance
+			WHERE testupgrade.id = testinstance.id
+		)
+		LIMIT 1000
+	)
+)
+`
+	result, err = pg.Exec(upgradeDeleteQuery)
+	if err != nil {
+		return -1, -1, fmt.Errorf("error deleting testupgrade entries: %v", err)
+	}
+	deleted, err = result.RowsAffected()
+	if err != nil {
+		return -1, -1, fmt.Errorf("error getting rows affected after deleting testupgrade entries: %v", err)
+	}
+	deletedRows += int(deleted)
+	log.Printf("Deleted %d testupgrade entries", deleted)
 
 	return prunedRows, deletedRows, nil
 }
